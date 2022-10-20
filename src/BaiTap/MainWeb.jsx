e056561a7a9c604190b9753465b496fba397a6f2
NgoThanhPhong <phongngo290902@gmail.com>
2022-10-20 22:29:37 +0700

    add

---
+++ src/BaiTap/MainWeb.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import DatGhe from './DatGhe.json'
+export default class MainWeb extends Component {
+    displayGhe=()=>{
+        return DatGhe.map((choNgoi,index)=>{
+            choNgoi
+        })
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
